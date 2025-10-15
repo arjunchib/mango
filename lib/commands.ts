2c166783d6a951a00d42112767ce255b187e3558
Arjun Chib <arjunpatrickchib@gmail.com>
2025-10-15 17:09:10 -0400

    Split commands and controllers

--- lib/commands.ts
+++ lib/commands.ts
@@ -5,11 +5,10 @@ import type {
   ChatInputCommandInteraction,
   ModalSubmitInteraction,
 } from "discord.js";
-import { watch } from "fs/promises";
+// import { watch } from "fs/promises";
 import type { PossiblePromise } from "./helpers";
 
 export interface Command {
-  command: APIApplicationCommand;
   default: new () => {
     onChatInput?: (
       interaction: ChatInputCommandInteraction
@@ -23,30 +22,31 @@ export interface Command {
   };
 }
 
-const commands = new Map<string, Command>();
-
 export async function findCommands() {
+  const commands = [];
   const glob = new Glob("./app/commands/*.tsx");
   for await (const file of glob.scan({ absolute: true })) {
-    const command = (await import(file)) as Command;
-    if (command.command) {
-      commands.set(command.command.name, command);
+    const command = (await import(file)) as { default: APIApplicationCommand };
+    if (command.default) {
+      commands.push(command.default);
     }
   }
   return commands;
 }
 
-export async function findCommand(name: string) {
-  return (await import(`${process.cwd()}/app/commands/${name}.tsx`)) as Command;
+export async function findController(name: string) {
+  return (await import(
+    `${process.cwd()}/app/controllers/${name}.tsx`
+  )) as Command;
 }
 
-export async function watchCommands() {
-  const watcher = watch("./app/commands/");
-  for await (const event of watcher) {
-    const { eventType, filename } = event;
-    console.log(`Detected ${eventType} in ${filename}`);
-    if (filename) {
-      await import(`${process.cwd()}/app/commands/${filename}`);
-    }
-  }
-}
+// export async function watchCommands() {
+//   const watcher = watch("./app/commands/");
+//   for await (const event of watcher) {
+//     const { eventType, filename } = event;
+//     console.log(`Detected ${eventType} in ${filename}`);
+//     if (filename) {
+//       await import(`${process.cwd()}/app/commands/${filename}`);
+//     }
+//   }
+// }
--- lib/push.ts
+++ lib/push.ts
@@ -4,10 +4,8 @@ import { findCommands } from "./commands";
 const rest = new REST().setToken(Bun.env.TOKEN!);
 
 export async function push() {
-  const commands = await findCommands();
-  const body = [...commands.values()].map((c) => c.command);
   await rest.put(
     Routes.applicationGuildCommands(Bun.env.APPLICATION_ID!, Bun.env.GUILD_ID!),
-    { body }
+    { body: await findCommands() }
   );
 }
